b2ac74513421f7368b7c6606ee9429faaa9a8e94
StevenGolovkine <steven_golovkine@icloud.com>
2024-02-14 09:54:58 +0100

    Logement plot

--- scripts/logements.R
+++ scripts/logements.R
@@ -24,23 +24,24 @@ df <- fr |>
 # Plot France
 myPalette <- RColorBrewer::brewer.pal(8, "PuRd")
 names(myPalette) <- levels(df$n_logement_bin)
-col_scale <- scale_fill_manual(name = "n_logement_bin", values = myPalette)
+LABELS <- paste0("< ", seq(100, 800, 100), "K")
+col_scale <- scale_fill_manual(
+    name = "n_logement_bin", values = myPalette,
+    labels = LABELS, drop = FALSE
+)
+
 
 # Metropolitan
 fr_metro <- df |> filter(type == 'Metropolitan département')
 fr_metro_gg <- ggplot() +
     geom_sf(data = fr_metro, aes(fill = n_logement_bin)) +
     col_scale +
-    labs(
-        title = "Nombre de logements ordinaires en France en 2020",
-        subtitle = "Source: INSEE.fr — Recensement 2020"
-    ) +
     theme_own() +
     theme(
         legend.title = element_blank(),
         legend.text = element_text(size = 6, family = "Courier"),
-        plot.title = element_text(size = 8, family = "Courier"),
-        plot.subtitle = element_text(size = 6, family = "Courier")
+        legend.position = c(0.1, 0.25),
+        legend.key.size = unit(0.5, "line")
     )
 
 # La Réunion
@@ -49,7 +50,9 @@ reunion_gg <- ggplot() +
     geom_sf(data = reunion, aes(fill = n_logement_bin)) +
     col_scale +
     theme_void() +
-    theme(legend.position = "none")
+    theme(
+        legend.position = "none"
+    )
 
 # Martinique
 martinique <- df |> filter(name == 'Martinique')
@@ -57,7 +60,9 @@ martinique_gg <- ggplot() +
     geom_sf(data = martinique, aes(fill = n_logement_bin)) +
     col_scale +
     theme_void() +
-    theme(legend.position = "none")
+    theme(
+        legend.position = "none"
+    )
 
 # Guadeloupe
 guadeloupe <- df |> filter(name == 'Guadeloupe')
@@ -65,7 +70,9 @@ guadeloupe_gg <- ggplot() +
     geom_sf(data = guadeloupe, aes(fill = n_logement_bin)) +
     col_scale +
     theme_void() +
-    theme(legend.position = "none")
+    theme(
+        legend.position = "none"
+    )
 
 # Guyane
 guyane <- df |> filter(name == 'Guyane française')
@@ -73,7 +80,9 @@ guyane_gg <- ggplot() +
     geom_sf(data = guyane, aes(fill = n_logement_bin)) +
     col_scale +
     theme_void() +
-    theme(legend.position = "none")
+    theme(
+        legend.position = "none"
+    )
 
 
 # Arrange plots
@@ -84,12 +93,23 @@ seas <- ggarrange(
     ncol = 1, nrow = 4
 )
 
-ggarrange(
+pp <- ggarrange(
     plotlist = list(fr_metro_gg, seas),
-    common.legend = TRUE, legend = "bottom",
     ncol = 2, nrow = 1,
     widths = c(6, 1)
 )
+annotate_figure(
+    pp,
+    top = text_grob(
+        "Nombre de logements ordinaires en France en 2020",
+        family = "Courier",
+        size = 8
+    ),
+    bottom = text_grob(
+        "Source: INSEE.fr — Recensement 2020",
+        family = "Courier", hjust = 1, x = 1, size = 6
+    )
+) + theme(plot.margin = margin(0.1, 0.1, 0.1, 0.1, "cm")) 
 
 ggsave(
     "./figures/logement.png", width = 1080, height = 1080, units = "px"
